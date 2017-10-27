4a504fcd5ce4bfa8cad737bf3c3390a20323f766
Srinivaslancius <srinivas@lanciussolutions.com>
2017-10-27 15:45:29 +0530

    new chnages

--- send_email.php
+++ send_email.php
@@ -4,256 +4,231 @@ include "manage_webmaster/admin_includes/config.php";
 $id = $_GET['lid'];
 
 $mailto = "srinivas@lanciussolutions.com";
-$mailfrom = "harikanthnakka9@gmail.com";
-$mailsubject = "Email Attch";
+$mailfrom = "srinud8008@gmail.com";
+$mailsubject = "VGCS Service Details";
 /* break description content every after 100 character. */
 
 
 $content = '';
 
 $content .= '
-		<style>
-		table {
-		border-collapse: collapse;
-		}
-
-		table{
-		 width:800px;
-		 margin:0 auto;
-		}
-
-		td{
-		border: 1px solid #e2e2e2;
-		padding: 10px; 
-		max-width:520px;
-		word-wrap: break-word;
-		}
-
-
-		</style>
-
-		';
-/* you css */
-
-$content .= '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
     <style>
-	.container-fluid{
-	     padding-bottom:100px;}
-        .table {
-            display: table;
-            border: 1px solid #808080;
-            text-align: center;
-            border-top-left-radius: 3px;
-            border-top-right-radius: 3px;
-            padding:0px;
-            margin-bottom: 0;
-			
-        }
+    table {
+    border-collapse: collapse;
+    }
 
-        .table-row {
-            display: table-row;
-        }
+    table{
+     width:800px;
+     margin:0 auto;
+    }
 
-        .table-cell {
-            display: table-cell;
-            border-bottom: 1px solid #b2b2b2;
-          padding-top:7px;
-		  padding-bottom:7px;
-		  font-size:13px;
-        }
-        .table-header {
-            font-weight: bold;
-            background-color: #d8d8d8;
-        }
-		.text{
-		 font-weight: bold;
-         background-color: #d8d8d8;}
+    td{
+    border: 1px solid #e2e2e2;
+    padding: 10px; 
+    max-width:520px;
+    word-wrap: break-word;
+    }
+
+
+    </style>
+
+    ';
+
+$content .= '<style>
+    .container{
+border:2px solid gray;
+width:1000px;
+padding-left:0px;
+margin-bottom:50px;
+}
+th, td {
+    text-align: left;
+    border-bottom:1px solid gray;
+    }
+    
+    th{
+    background-color:#d8d8d8;
+    text-align:center;
+    }
     </style>';
 
-$content .= '<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>VENUS GENIE CARE SERVICES</title
-</head>
 
-<body>
-    <br><br>
-    <div class="container-fluid">
-        <div class="row"> 
-		<div class="col-sm-2">
-		</div>
-            <div class="col-sm-8">
-                <div class="table">
-                    <div class="table-row table-header">
-                        <div class="table-cell">VGCS</div> 
-						<div class="table-cell"></div>
-						<div class="table-cell">VENUS GENIE CARE SERVICES</div>
-                        <div class="table-cell"></div>
-                        <div class="table-cell">Mobile: +91-9849257587<br>mail:venugopal.7@hotmail.com</div>
-                    </div>
-                    <div class="table-row">
-                        <div class="table-cell">REPORT TYPE:</div>
-                        <div class="table-cell">AMC REPORT</div>
-						<div class="table-cell">SERVICE REPORT</div>
-                        <div class="table-cell">SL.NO.</div>
-                        <div class="table-cell">Date:</div>
-                    </div>
-                    <div class="table-row">
-                        <div class="table-cell">Customer Name:</div>
-						<div class="table-cell"></div>
-                        <div class="table-cell">Email:</div>
-                        <div class="table-cell"></div>
-                        <div class="table-cell">Cell:</div>
-                    </div>
-                    <div class="table-row">
-                        <div class="table-cell">VISIT TYPE:</div>
-                        <div class="table-cell">Commissioning Visit</div>
-                        <div class="table-cell">Paid Visit</div>
-                        <div class="table-cell">Break Down Visit</div>
-						<div class="table-cell">AMC Visit</div>
-                    </div>
-                    <div class="table-row">
-                        <div class="table-cell">ENG/Eqp Id:</div>
-						 <div class="table-cell"></div>
-                        <div class="table-cell">ALT./MODEL:</div>
-						 <div class="table-cell"></div>
-                        <div class="table-cell">RATING/KVA:</div>                       
-                    </div>
-					 <div class="table-row">
-                        <div class="table-cell">ENG/SI.No:</div>
-						 <div class="table-cell"></div>
-                        <div class="table-cell">ALT./SI.No:</div>
-						 <div class="table-cell"></div>
-                        <div class="table-cell">PANNEL/TYPE:</div>                       
-                    </div>
-					<div class="table-row">
-                        <div class="table-cell">PM/DATE:</div>
-						 <div class="table-cell"></div>
-                        <div class="table-cell">PM/TYPE:</div>
-						 <div class="table-cell"></div>
-                        <div class="table-cell">RUN/HRS:</div>                       
-                    </div>
-					<div class="table-row">
-                        <div class="table-cell">DATE OF INFORMED:</div>
-						 <div class="table-cell"></div>
-                        <div class="table-cell">DATE OF VISIT:</div>
-						 <div class="table-cell"></div>
-                        <div class="table-cell">LOCATION:</div>                       
-                    </div>
-					<div class="table-row">
-                        <div class="table-cell">AMC VISIT CHECK LIST </div>
-						 <div class="table-cell"></div>
-                        <div class="table-cell"></div>
-						 <div class="table-cell"></div>
-                        <div class="table-cell">SERVICE VISIT CHECK LIST</div>                       
-                    </div>
-					<div class="table-row">
-                        <div class="table-cell">Water Temp</div>
-						 <div class="table-cell">Fuel Pressure</div>
-                        <div class="table-cell">Oil Pressure</div>
-						 <div class="table-cell">Battery chg</div>
-                        <div class="table-cell"></div>                       
-                    </div>
-					<div class="table-row">
-                        <div class="table-cell">Speed/Freq</div>
-						 <div class="table-cell">Load in kw</div>
-                        <div class="table-cell">Load in AMPS</div>
-						 <div class="table-cell">Voltage</div>
-                        <div class="table-cell"></div>                       
-                    </div>
-					<div class="table-row">
-                        <div class="table-cell">Check Oil level / Condition</div>
-						 <div class="table-cell"></div>
-                        <div class="table-cell">Check Radiator all Houses/Condition</div>
-						 <div class="table-cell"></div>
-                        <div class="table-cell">Check Breather / Clean</div>                       
-                    </div>
-					<div class="table-row">
-                        <div class="table-cell">Check Fuel / Condition</div>
-						 <div class="table-cell"></div>
-                        <div class="table-cell">Check Drive Belts / Condition</div>
-						 <div class="table-cell"></div>
-                        <div class="table-cell">Check Air Filters / Clean</div>                       
-                    </div>
-					<div class="table-row">
-                        <div class="table-cell">Check Coolent/ Condition</div>
-						 <div class="table-cell"></div>
-                        <div class="table-cell">CCheck if any / Leakages</div>
-						 <div class="table-cell"></div>
-                        <div class="table-cell">Check Valve Injector / Adjust</div>                       
-                    </div>
-					<div class="table-row">
-                        <div class="table-cell">Check Battery / Condition</div>
-						 <div class="table-cell"></div>
-                        <div class="table-cell">Start Check Exhaust Smoke / Limits</div>
-						 <div class="table-cell"></div>
-                        <div class="table-cell">Start Check Abnormal Sounds/Observe</div>                       
-                    </div>
-					<div class="table-row">
-                        <div class="table-cell">AMC VISIT CHECK LIST Comments if any:</div>
-						 <div class="table-cell"></div>
-                        <div class="table-cell"></div>
-						 <div class="table-cell"></div>
-                        <div class="table-cell"></div>                       
-                    </div>
-					<div class="table-row">
-                        <div class="table-cell">FAILURE DETAILS:</div>
-						 <div class="table-cell"></div>
-                        <div class="table-cell"></div>
-						 <div class="table-cell"></div>
-                        <div class="table-cell"></div>                       
-                    </div>
-					<div class="table-row">
-                        <div class="table-cell">CAUSE OF FAILURE:</div>
-						 <div class="table-cell"></div>
-                        <div class="table-cell"></div>
-						 <div class="table-cell"></div>
-                        <div class="table-cell"></div>                       
-                    </div>
-					<div class="table-row">
-                        <div class="table-cell">WORK CARRIED OUT:</div>
-						 <div class="table-cell"></div>
-                        <div class="table-cell"></div>
-						 <div class="table-cell"></div>
-                        <div class="table-cell"></div>                       
-                    </div>
-					<div class="table-row">
-                        <div class="table-cell">CUSTOMER COMMENTS:</div>
-						 <div class="table-cell"></div>
-                        <div class="table-cell"></div>
-						 <div class="table-cell"></div>
-                        <div class="table-cell"></div>                       
-                    </div>   
-					<div class="table-row">
-                        <div class="table-cell">INCHARGE</div>
-						 <div class="table-cell"></div>
-                        <div class="table-cell"></div>
-						 <div class="table-cell"></div>
-                        <div class="table-cell">SERVICE ENGINEER</div>                       
-                    </div>   					
-                </div>
-            </div>
-			<div class="col-sm-2">
-        </div>
-    </div>
-	</div>
-</body>
-</html>';
+$content .= '<div class="container" style="padding-right:0px">         
+  <table class="table">
+    <thead>
+      <tr>
+        <th>VGCS</th>
+        <th></th>
+        <th>VENUS GENIE CARE SERVICES</th>
+        <th></th>
+        <th>MOBILE:+91-9849257587<br>E-mail:venugopal.7@hotmail.com</th>        
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>REPORT TYPE:</td>
+        <td>AMC REPORT</td>
+        <td></td>
+        <td>SL.NO.</td>
+        <td>Date:</td>
 
-                //echo $content; die;
+      </tr>
+       <tr>
+        <td>Customer Name:</td>
+        <td></td>
+        <td>Email:</td>
+        <td></td>
+        <td>Cell:</td>
+      </tr>
+        <tr>
+        <td>VISIT TYPE:</td>
+        <td>Commissioning Visit</td>
+        <td>Paid Visit</td>
+        <td>Break Down Visit</td>
+        <td>AMC Visit</td>
+      </tr>
+      <tr>
+        <td>ENG/Eqp Id:</td>
+        <td></td>
+        <td>ALT./MODEL:</td>
+        <td></td>
+        <td>RATING/KVA:</td>
+      </tr>
+      <tr>
+        <td>ENG/SI.No:</td>
+        <td></td>
+        <td>ALT./SI.No:</td>
+        <td></td>
+        <td>PANNEL/TYPE:</td>
+      </tr>
+       <tr>
+        <td>PM/DATE:</td>
+        <td></td>
+        <td>PM/TYPE:</td>
+        <td></td>
+        <td>RUN/HRS:</td>
+      </tr>
+      <tr>
+        <td>DATE OF INFORMED:</td>
+        <td></td>
+        <td>DATE OF VISIT:</td>
+        <td></td>
+        <td>LOCATION:</td>
+      </tr>
+      <tr>
+        <td>AMC VISIT CHECK LIST </td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td>SERVICE VISIT CHECK LIST</td>
+      </tr>
+       <tr>
+        <td>Water Temp</td>
+        <td>Fuel Pressure</td>
+        <td>Oil Pressure</td>
+        <td>Battery chg</td>
+        <td></td>
+      </tr>
+       <tr>
+        <td>Speed/Freq</td>
+        <td>Load in kw</td>
+        <td>Load in AMPS</td>
+        <td>Voltage</td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>Check Oil level / Condition</td>
+        <td></td>
+        <td>Check Radiator all Houses/Condition</td>
+        <td></td>
+        <td>Check Breather / Clean</td>
+      </tr>
+       <tr>
+        <td>Check Fuel / Condition</td>
+        <td></td>
+        <td>Check Drive Belts / Condition</td>
+        <td></td>
+        <td>Check Air Filters / Clean</td>
+      </tr>
+       <tr>
+        <td>Check Coolent / Condition</td>
+        <td></td>
+        <td>Check if any / Leakages</td>
+        <td></td>
+        <td>Check Valve Injector / Adjust</td>
+      </tr>
+       <tr>
+        <td>Check Battery / Condition</td>
+        <td></td>
+        <td>Start Check Exhaust Smoke / Limits</td>
+        <td></td>
+        <td>Start Check Abnormal Sounds/Observe</td>
+      </tr>
+      <tr>
+        <td>AMC VISIT CHECK LIST Comments if any:</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>FAILURE DETAILS:</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+       <tr>
+        <td>CAUSE OF FAILURE:</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+       <tr>
+        <td>WORK CARRIED OUT:</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+       <tr>
+        <td>CUSTOMER COMMENTS:</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+       <tr>
+        <td>INCHARGE</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td>SERVICE ENGINEER</td>
+      </tr>
+       <tr>
+        <td style="border-bottom:0px">SIGNATURE</td>
+        <td style="border-bottom:0px"></td>
+        <td style="border-bottom:0px"></td>
+        <td style="border-bottom:0px"></td>
+        <td style="border-bottom:0px">SIGNATURE</td>
+      </tr>
+    </tbody>
+  </table>
+</div>';
+
+
+//echo $content; die;
 
 require_once('html2pdf/html2pdf.class.php');
 
 
-$html2pdf = new HTML2PDF('P', 'A4', 'fr');
+$html2pdf = new HTML2PDF('P', 'A3', 'fr');
 
 $html2pdf->setDefaultFont('Arial');
 $html2pdf->writeHTML($content, isset($_GET['vuehtml']));
 
-$html2pdf = new HTML2PDF('P', 'A4', 'fr');
+$html2pdf = new HTML2PDF('P', 'A3', 'fr');
 $html2pdf->WriteHTML($content);
 
 
