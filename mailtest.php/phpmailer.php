14360793ab37d2c0dd1072b59d8b125f4e106900
Srinivaslancius <srinivas@lanciussolutions.com>
2017-11-11 15:55:15 +0530

    new pph am,il

---
+++ mailtest.php/phpmailer.php
@@ -0,0 +1,37 @@
+<?php
+
+require("class.phpmailer.php");
+
+$mail = new PHPMailer();
+
+$mail->IsSMTP();
+$mail->Host = "joinaslicagentinhyd.in";
+$mail->SMTPAuth = true;
+//$mail->SMTPSecure = "ssl";
+$mail->Port = 587;
+$mail->Username = "info@joinaslicagentinhyd.in";
+$mail->Password = "Admin@123";
+
+$mail->From = "info@joinaslicagentinhyd.in";
+$mail->FromName = "Test from Info";
+$mail->AddAddress("info@joinaslicagentinhyd.in");
+//$mail->AddReplyTo("mail@mail.com");
+
+$mail->IsHTML(true);
+
+$mail->Subject = "Test message from server";
+$mail->Body = "Test Mail<b>in bold!</b>";
+//$mail->AltBody = "This is the body in plain text for non-HTML mail clients";
+
+if(!$mail->Send())
+{
+echo "Message could not be sent. <p>";
+echo "Mailer Error: " . $mail->ErrorInfo;
+exit;
+}
+
+echo "Message has been sent";
+
+?>
+------------------------
+
